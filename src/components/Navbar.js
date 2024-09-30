a8bd06e6fb22ae761ba54a89858e9da9938a4596
dapijon <dapijon@gmail.com>
2024-09-30 10:05:48 +0300

    Navbar slider

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-// components/Navbar.js
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src="iconlogo.png" alt="LuxuTrade Logo" />
+        <span className="navbar-title">LuxuTrade</span>
       </div>
-      <ul className="navbar-links">
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#product">Products</a></li>
-      </ul>
+      <div className="navbar-toggle" onClick={handleToggle}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Side Panel for Mobile */}
+      <div className={`side-panel ${isOpen ? "active" : ""}`}>
+        <ul className="navbar-links">
+          <li><a href="#hero" onClick={handleToggle}>Home</a></li>
+          <li><a href="#about" onClick={handleToggle}>About</a></li>
+          <li><a href="#product" onClick={handleToggle}>Products</a></li>
+        </ul>
+      </div>
     </nav>
   );
 };
