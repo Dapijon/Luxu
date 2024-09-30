9a66ad1a93000f8469bfe5656bf1c134fa4a2222
dapijon <dapijon@gmail.com>
2024-09-30 08:30:17 +0300

    increased logo size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,13 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src="iconlogo.ico" alt="LuxuTrade Logo" />
+        <img src="iconlogo.png" alt="LuxuTrade Logo" />
       </div>
       <ul className="navbar-links">
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#product">Products</a></li>
       </ul>
-      <button className="contact-btn">
-        <a href="#contact">Contact</a>
-      </button>
     </nav>
   );
 };
