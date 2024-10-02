4d306aac0063ae98e65a049cdb93c51d4ad1494c
dapijon <dapijon@gmail.com>
2024-10-02 14:58:15 +0300

    Added the loading animations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 // App.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Product from "./components/Product"
+import Product from "./components/Product";
 import ContactUs from "./components/ContactUs";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import Loading from "./components/Loading"; 
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <About />
-      <Product />
-      <ContactUs />
-      <Footer />
+      {loading && <Loading />}
+      {!loading && (
+        <>
+          <Navbar />
+          <Hero />
+          <About />
+          <Product />
+          <ContactUs />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css'; 
+
+const Loading = () => {
+    return (
+        <div id="loading">
+            <img src="iconlogo.png" alt="Company Logo" />
+        </div>
+    );
+};
+
+export default Loading;
