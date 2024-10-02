149513b6d333eccfbabbae93df3108dba5fa38bf
dapijon <dapijon@gmail.com>
2024-10-02 22:49:24 +0300

    Adjusted Loading transition duration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -6,15 +7,16 @@ import About from "./components/About";
 import Product from "./components/Product";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
-import Loading from "./components/Loading"; 
+import Loading from "./components/Loading"; // Import the Loading component
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); 
+      document.getElementById('loading').classList.add('fade-out');
+      setTimeout(() => setLoading(false), 500); // Match the duration of the CSS transition
+    }, 3500); // Adjust the delay time as needed (2000ms = 2 seconds)
 
     return () => clearTimeout(timer);
   }, []);
